ec770ba8d2321b621b9f1fd62b14b08759595aa4
oliver <oliverguo29@gmail.com>
2019-10-22 22:40:09 +0100

    behaviour

--- src/create/singleton/TestSingleton_lazy.java
+++ src/create/singleton/TestSingleton_lazy.java
@@ -3,9 +3,11 @@ package create.singleton;
 /**
  *      单例模式： 懒汉型
  *      线程不安全，除非加锁
+ *
+ *      双重校验
  */
 public class TestSingleton_lazy {
-    private static  TestSingleton_lazy uniqueInstance;
+    private volatile static   TestSingleton_lazy uniqueInstance;
 
     private TestSingleton_lazy(){
 
@@ -13,7 +15,11 @@ public class TestSingleton_lazy {
 
     public  static synchronized  TestSingleton_lazy getInstance(){      //加锁，效率低
         if(uniqueInstance==null){
-            uniqueInstance=new TestSingleton_lazy();
+            synchronized (TestSingleton_lazy.class){
+                if(uniqueInstance==null){
+                    uniqueInstance=new TestSingleton_lazy();
+                }
+            }
         }
         return uniqueInstance;
     }
